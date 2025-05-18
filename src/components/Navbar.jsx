9266b5b74e60063f0be1b3071b923d6e3403de34
SivaCortexCoder <sivaofficial76@gmail.com>
2025-05-18 18:09:06 +0530

    updated text-issue  files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,10 @@ const Navbar = () => {
     <div>
         <nav className=' bg-[#111827]/50 flex justify-between items-center p-5 border-b border-gray-500'>
             <div>
-                <h1 className='text-white flex items-center gap-2 font-semibold text-sm md:text-lg lg:text-xl'><FaTasks/>Task Management App</h1>
+                <h1 className='text-white  flex items-center gap-2 font-semibold text-xs md:text-lg lg:text-xl'><FaTasks/>Task Management App</h1>
 
             </div>
-            <div className='text-white flex gap-5 items-center text-sm md:text-lg lg:text-xl '>   
+            <div className='text-white flex gap-5 items-center text-xs md:text-lg lg:text-xl '>   
                <Link to='/'>Home</Link>
                 <h2>Task Overview</h2>
 
